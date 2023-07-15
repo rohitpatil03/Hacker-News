47bce05d61d6d748936934b1f15ba1e5ed46c7df
rohit <rohit@ubuntu.myguest.virtualbox.org>
2023-07-16 00:25:33 +0530

    Added Search Feature

---
+++ app.js
@@ -0,0 +1,27 @@
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+
+const fetchData = async (url) => {
+    const resp = await fetch(url);
+    const resData = await resp.json();
+    console.log(resData.hits)
+    return resData.hits
+}
+
+
+const app = Vue.createApp({
+    data(){
+        return({
+            formInput:"Vue",
+            data:[],
+    });
+    },
+    methods:{
+        async updateSearchBar(event){
+            this.formInput = event.target.value;
+            this.data = await fetchData(`${API_ENDPOINT}query=${this.formInput}&page=1`);
+        },
+        
+    }
+})
+
+app.mount('#hacker-news')
