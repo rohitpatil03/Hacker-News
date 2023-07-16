32d005b3c953b3b0035b6ddd0bf066cad666ceeb
rohit <rohit@ubuntu.myguest.virtualbox.org>
2023-07-16 09:08:13 +0530

    Added Next/Prev Button

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 const fetchData = async (url) => {
     const resp = await fetch(url);
     const resData = await resp.json();
-    console.log(resData.hits)
-    return resData.hits
+    return resData
 }
 
 
@@ -13,20 +12,47 @@ const app = Vue.createApp({
         return({
             formInput:"Vue",
             data:[],
+            pageNumber:0,
+            nbPages:1,
     });
     },
     methods:{
         async updateSearchBar(event){
             this.formInput = event.target.value;
-            this.data = await fetchData(`${API_ENDPOINT}query=${this.formInput}&page=1`);
+            await this.updateData()
+        },
+        async updateData(){
+            response = await fetchData(`${API_ENDPOINT}query=${this.formInput}&page=${this.pageNumber}`);
+            this.nbPages = await response.nbPages
+            this.data = await response.hits
         },
         removeBtn(id){
             this.data = this.data.filter(item => item.objectID !== id)
-        }
+        },
+        setPageNumber(command){
+            if (command == 'prev' && this.pageNumber<=1){
+                this.pageNumber = this.nbPages-1
+                this.updateData()
+            }
+            else if (command == 'prev' && this.pageNumber>1){
+                this.pageNumber = this.pageNumber - 1
+                this.updateData()
+            }
+            else if(command == 'next' && this.pageNumber>=this.nbPages-1){
+                this.pageNumber = 0
+                this.updateData()
+            }
+            else if (command == 'next' && this.pageNumber<this.nbPages-1){
+                this.pageNumber = this.pageNumber + 1
+                this.updateData()
+            }
+            
+            
+        },
         
     },
     async mounted() {
-        this.data = await fetchData(`${API_ENDPOINT}query=${this.formInput}&page=1`);
+        await this.updateData()
       }
 })
 
